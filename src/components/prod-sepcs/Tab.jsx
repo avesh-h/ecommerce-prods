1ec86acad36b3bd0900d3b52c27649ab6498e9ca
Avesh <aveshhasanfatta1155@gmail.com>
2025-01-11 22:13:44 +0530

    change responsive in the tab.jsx

--- src/components/prod-sepcs/Tab.jsx
+++ src/components/prod-sepcs/Tab.jsx
@@ -13,7 +13,7 @@ const Tab = () => {
             <li key={tab} className="text-nowrap">
               <button
                 onClick={() => setActiveTab(tab)}
-                className={`inline-block sm:p-4 p-[11px] rounded-t-lg ${
+                className={`inline-block sm:p-4 px-2.5 py-1 rounded-t-lg ${
                   activeTab === tab
                     ? "text-black border-b-4 border-red-500 font-semibold"
                     : "text-gray-500 border-b-4 border-transparent hover:text-gray-700 hover:border-gray-300"
@@ -25,12 +25,12 @@ const Tab = () => {
           ))}
         </ul>
       </div>
-      <div className="pt-6 text-gray-700">
-        <div className="columns-1 md:columns-2 gap-8">
-          <p>{prodSpecContent[activeTab]?.details}</p>
+      <div className="sm:pt-6 pt-4 text-gray-700">
+        <div className="columns-1 md:columns-2 gap-8 sm:text-base text-sm">
+          <p className="mb-2 sm:mb-0">{prodSpecContent[activeTab]?.details}</p>
           <ul className="list-disc pl-6 text-gray-700">
             {prodSpecContent[activeTab]?.features?.map((feature, index) => (
-              <li key={index} className="mb-2">
+              <li key={index} className="mb-2 sm:pt-0.5 pt-2">
                 {feature}
               </li>
             ))}
