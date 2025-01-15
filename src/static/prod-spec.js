5ed8dccfa8436110212d00bf82b74af9bba9480e
Avesh <aveshhasanfatta1155@gmail.com>
2025-01-15 20:00:54 +0530

    flicker issue

--- src/static/prod-spec.js
+++ src/static/prod-spec.js
@@ -15,27 +15,42 @@ export const prodSpecContent = {
     ],
   },
   Specs: {
-    details: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+    details: `
+    Duro Kent Flask is a Vacusteel Water Bottle from Cello's adventurous Duro Series of Tuff Steel insulated bottles. Designed for those with an adventurous spirit, this flask in sleek black, cool blue, vibrant green, and bold red is the perfect companion for treks, hikes, cycling, gym sessions, school, or office days. Engineered with new and advanced DTP coating, the bottle's vibrant color remains durable, resisting fading or scratching even with daily use. With double-wall insulation and Vacusteel Technology featuring a copper-coated interior, it keeps hot water piping hot and cold water refreshingly cold for extended periods. This leak-proof and odor-free bottle ensures your beverages are stored without oxidation or unpleasant smells, all while remaining rust-free. With a convenient loop for easy carrying, the Duro Kent Flask offers both style and functionality for your active lifestyle`,
     features: [
-      `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock.`,
-      `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock.`,
-      `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock.`,
+      `The Duro Kent Flask is a versatile companion, suitable for treks, hikes, cycling, gym sessions, school, or office uses.`,
+      `Embrace an active lifestyle while your flask maintains its brand-new appearance, even with daily wear and tear.`,
+      `The flask is designed to be leak-proof and odor-free, ensuring your beverages stay fresh and free from unpleasant smells, oxidation, or rust. It's equipped with a convenient loop for easy carrying during your outdoor adventures or daily activities.`,
+      `Enjoy the benefits of double-wall insulation, which keeps hot beverages piping hot and cold drinks refreshingly cold for extended periods. Utilizing Vacusteel technology, this flask features double stainless walls with a copper coating on the inside, minimizing temperature loss.
+  `,
+      `Meet the Duro Kent Flask, a Vacusteel Water Bottle from Cello's adventure-ready Duro Series of Tuff Steel insulated bottles, available in sleek black, cool blue, vibrant green, and bold red, perfect for the adventurous spirit in you.
+  `,
     ],
   },
   "Return Policy": {
-    details: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`,
+    details: `
+    Duro Kent Flask is a Vacusteel Water Bottle from Cello's adventurous Duro Series of Tuff Steel insulated bottles. Designed for those with an adventurous spirit, this flask in sleek black, cool blue, vibrant green, and bold red is the perfect companion for treks, hikes, cycling, gym sessions, school, or office days. Engineered with new and advanced DTP coating, the bottle's vibrant color remains durable, resisting fading or scratching even with daily use. With double-wall insulation and Vacusteel Technology featuring a copper-coated interior, it keeps hot water piping hot and cold water refreshingly cold for extended periods. This leak-proof and odor-free bottle ensures your beverages are stored without oxidation or unpleasant smells, all while remaining rust-free. With a convenient loop for easy carrying, the Duro Kent Flask offers both style and functionality for your active lifestyle`,
     features: [
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
-      `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock.`,
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+      `The Duro Kent Flask is a versatile companion, suitable for treks, hikes, cycling, gym sessions, school, or office uses.`,
+      `Embrace an active lifestyle while your flask maintains its brand-new appearance, even with daily wear and tear.`,
+      `The flask is designed to be leak-proof and odor-free, ensuring your beverages stay fresh and free from unpleasant smells, oxidation, or rust. It's equipped with a convenient loop for easy carrying during your outdoor adventures or daily activities.`,
+      `Enjoy the benefits of double-wall insulation, which keeps hot beverages piping hot and cold drinks refreshingly cold for extended periods. Utilizing Vacusteel technology, this flask features double stainless walls with a copper coating on the inside, minimizing temperature loss.
+  `,
+      `Meet the Duro Kent Flask, a Vacusteel Water Bottle from Cello's adventure-ready Duro Series of Tuff Steel insulated bottles, available in sleek black, cool blue, vibrant green, and bold red, perfect for the adventurous spirit in you.
+  `,
     ],
   },
   Reviews: {
-    details: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.`,
+    details: `
+    Duro Kent Flask is a Vacusteel Water Bottle from Cello's adventurous Duro Series of Tuff Steel insulated bottles. Designed for those with an adventurous spirit, this flask in sleek black, cool blue, vibrant green, and bold red is the perfect companion for treks, hikes, cycling, gym sessions, school, or office days. Engineered with new and advanced DTP coating, the bottle's vibrant color remains durable, resisting fading or scratching even with daily use. With double-wall insulation and Vacusteel Technology featuring a copper-coated interior, it keeps hot water piping hot and cold water refreshingly cold for extended periods. This leak-proof and odor-free bottle ensures your beverages are stored without oxidation or unpleasant smells, all while remaining rust-free. With a convenient loop for easy carrying, the Duro Kent Flask offers both style and functionality for your active lifestyle`,
     features: [
-      `At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.`,
-      `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock.`,
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+      `The Duro Kent Flask is a versatile companion, suitable for treks, hikes, cycling, gym sessions, school, or office uses.`,
+      `Embrace an active lifestyle while your flask maintains its brand-new appearance, even with daily wear and tear.`,
+      `The flask is designed to be leak-proof and odor-free, ensuring your beverages stay fresh and free from unpleasant smells, oxidation, or rust. It's equipped with a convenient loop for easy carrying during your outdoor adventures or daily activities.`,
+      `Enjoy the benefits of double-wall insulation, which keeps hot beverages piping hot and cold drinks refreshingly cold for extended periods. Utilizing Vacusteel technology, this flask features double stainless walls with a copper coating on the inside, minimizing temperature loss.
+  `,
+      `Meet the Duro Kent Flask, a Vacusteel Water Bottle from Cello's adventure-ready Duro Series of Tuff Steel insulated bottles, available in sleek black, cool blue, vibrant green, and bold red, perfect for the adventurous spirit in you.
+  `,
     ],
   },
 };
